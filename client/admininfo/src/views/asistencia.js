089aaea22277f401151365eb7c32272ceb3c4126
lgoenaga <luis.goenaga@est.iudigital.edu.co>
2023-03-03 00:36:59 -0500

    Paginación de Usuarios

--- client/admininfo/src/views/asistencia.js
+++ client/admininfo/src/views/asistencia.js
@@ -1,13 +1,14 @@
 import React from "react";
 import BarraNavegacion from "../components/header";
 import Footer from "../components/footer";
-
+import AdvancedExample from "../components/pagebasic"
 
 const ViewAsistencia = () => {
   return (
     <>
       <BarraNavegacion/>
       <h3>Vista de Asistencia</h3>
+      <AdvancedExample />
       <Footer/>
     </>
   );
--- client/admininfo/src/views/usuarios/usuarioslistar.js
+++ client/admininfo/src/views/usuarios/usuarioslistar.js
@@ -2,15 +2,11 @@ import React from "react";
 import { ListUsuarios } from "../../services/usuarios/usuarioslistar";
 import BarraNavegacion from "../../components/header";
 import Footer from "../../components/footer";
-import paginationBasic from "../../components/page";
-
-paginationBasic();
 
 const ViewUsuarios = () => {
   return (
     <>
       <BarraNavegacion />
-
       <ListUsuarios />
 
       <Footer />
