0227bbe94ae2bdd644cafd0289b7b80660964a9d
lgoenaga <luis.goenaga@est.iudigital.edu.co>
2023-03-05 17:26:18 -0500

    Actualización del puerto en el index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
-const port = process.env.NODE_PORT;
+const port = process.env.NODE_PORT | 4005;
 const host = process.env.NODE_HOST;
 
 const path = require("path");
@@ -15,23 +15,18 @@ getConexion();
 
 app.use(express.json());
 
-
-app.use(
-  express.static(path.resolve(__dirname))
-);
+app.use(express.static(path.resolve(__dirname)));
 
 app.use("/contactos", require("./routers/contactos"));
 app.use("/usuarios", require("./routers/usuarios"));
-app.use("/login", require ("./routers/login"));
+app.use("/login", require("./routers/login"));
 
 // Todas las peticiones GET que no hayamos manejado en las líneas anteriores retornaran nuestro app React
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname)
-  );
+  res.sendFile(path.resolve(__dirname));
 });
 
 app.listen(port, () => {
-   console.clear();
+  console.clear();
   console.log(`Server ${host} listening on port ${port}`);
 });
